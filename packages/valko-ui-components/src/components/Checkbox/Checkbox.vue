76b8c1fdaf91ed95f0201e7b8602c310b7409eba
Exequiel <80073050+BabyDr1v3r@users.noreply.github.com>
2024-01-16 17:32:36 -0300

    fix(vui-4): fix checkbox component properties & styles (#34)

--- packages/valko-ui-components/src/components/Checkbox/Checkbox.vue
+++ packages/valko-ui-components/src/components/Checkbox/Checkbox.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import { CheckboxProps } from '@/components/Checkbox/interfaces'
+import { VkIcon } from '@/components/Icon'
 import useStyle from './Checkbox.styles'
 
 defineOptions({ name: 'VkCheckbox' })
 
 const props = withDefaults(defineProps<CheckboxProps>(), {
   color: 'primary',
-  modelValue: false,
-  indeterminate: false,
   variant: 'filled',
   size: 'md',
   label: 'Checkbox',
+  modelValue: false,
+  indeterminate: false,
   readonly: false
 })
 
@@ -26,56 +27,37 @@ const classes = useStyle(props)
 </script>
 
 <template>
-  <div @click="onClick">
-    <div :class="classes.label">
+  <div>
+    <div
+      :class="classes.container"
+      @click="onClick"
+    >
       <div
         :class="classes.checkbox"
-        :data-checked="modelValue === true"
+        :data-checked="!!modelValue"
         :data-indeterminate="modelValue === null"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="4"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          v-if="modelValue !== false"
-        >
-          <path
-            :class="classes.svg"
-            d="M5 13l4 4L19 7"
-            v-if="modelValue"
-          />
-          <line
-            :class="classes.svg"
-            v-else
-            x1="5"
-            y1="12"
-            x2="19"
-            y2="12"
-          />
-        </svg>
+        <vk-icon
+          v-if="modelValue || props.indeterminate"
+          :name="`${props.indeterminate ? 'minus' : 'check'}`"
+          :class="classes.icon"
+        />
       </div>
       <input
         @click.prevent=""
         type="checkbox"
-        :checked="modelValue === true"
+        :checked="!!modelValue"
         :class="classes.input"
         :indeterminate="modelValue === null"
         :helpertext="props.helpertext"
       >
-      <label
-        class="align-middle"
-        :class="classes.label"
-      >
+      <label :class="classes.label">
         {{ label }}
       </label>
     </div>
     <span
       v-if="props.helpertext"
-      :class="classes.message"
+      :class="classes.helpertext"
     >
       {{ props.helpertext }}
     </span>
--- packages/valko-ui-components/src/components/Checkbox/interfaces.ts
+++ packages/valko-ui-components/src/components/Checkbox/interfaces.ts
@@ -3,13 +3,13 @@ import Colors from '@/types/colors'
 import Variants from '@/types/variants'
 
 export interface CheckboxProps extends Colors, Sizes, Variants  {
+  helpertext?: string;
+  label?: string;
+  modelValue: boolean | null;
   rounded?: boolean;
   disabled?: boolean;
   loading?: boolean;
   flat?: boolean;
-  modelValue: boolean | null;
-  label?: string;
-  helpertext?: string;
   labelPosition?: boolean;
   readonly?: boolean;
   indeterminate?: boolean;
