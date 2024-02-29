4ebf9f81ed92395c7a3edecfcfa954b58ccbabae
Exequiel <80073050+BabyDr1v3r@users.noreply.github.com>
2024-02-29 16:13:03 -0300

    feat(vui-52): add badge component (#53)

---
+++ packages/valko-ui-components/src/components/Badge/Badge.spec.ts
@@ -0,0 +1,205 @@
+import { VueWrapper, mount } from '@vue/test-utils'
+import { VkBadge } from '.'
+
+describe('Avatar component', () => {
+  let wrapper: VueWrapper
+  describe('Props', () => {
+    describe('With default props', () => {
+      beforeEach(() => {
+        wrapper = mount(VkBadge, {})
+      })
+
+      it('should render', () => {
+        expect(wrapper.find('.vk-badge').exists()).toBe(true)
+      })
+
+      it('should be color primary', () => {
+        expect(wrapper.find('.bg-primary-500').exists()).toBe(true)
+      })
+
+      it('should be size md', () => {
+        expect(wrapper.find('.vk-badge__content').classes()).toContain('text-sm')
+      })
+
+      it('should be shape soft', () => {
+        expect(wrapper.find('.rounded-lg').exists()).toBe(true)
+      })
+
+      it('should be displayed on top-right', () => {
+        expect(wrapper.find('.right-0').exists()).toBe(true)
+      })
+
+      it('should not be flat', () => {
+        expect(wrapper.find('.shadow-none').exists()).toBe(false)
+      })
+    })
+
+    describe('When color prop changes', () => {
+      it('should be color primary when props.color is primary', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'primary'
+          }
+        })
+
+        expect(wrapper.find('.bg-primary-500').exists()).toBe(true)
+      })
+
+      it('should be color secondary when props.color is secondary', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'secondary'
+          }
+        })
+
+        expect(wrapper.find('.bg-secondary-500').exists()).toBe(true)
+      })
+
+      it('should be color success when props.color is success', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'success'
+          }
+        })
+
+        expect(wrapper.find('.bg-success-500').exists()).toBe(true)
+      })
+
+      it('should be color info when props.color is info', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'info'
+          }
+        })
+
+        expect(wrapper.find('.bg-info-500').exists()).toBe(true)
+      })
+
+      it('should be color warning when props.color is warning', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'warning'
+          }
+        })
+
+        expect(wrapper.find('.bg-warning-500').exists()).toBe(true)
+      })
+
+      it('should be color error when props.color is error', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'error'
+          }
+        })
+
+        expect(wrapper.find('.bg-error-500').exists()).toBe(true)
+      })
+
+      it('should be color light when props.color is light', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'light'
+          }
+        })
+
+        expect(wrapper.find('.bg-light-1').exists()).toBe(true)
+      })
+
+      it('should be color dark when props.color is dark', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            color: 'dark'
+          }
+        })
+
+        expect(wrapper.find('.bg-dark-5').exists()).toBe(true)
+      })
+    })
+
+    describe('When shape prop changes', () => {
+      it('should be rounded when props.shape is rounded', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            shape: 'rounded'
+          }
+        })
+
+        expect(wrapper.find('.rounded-full').exists()).toBe(true)
+      })
+
+      it('should be soft when props.shape is soft', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            shape: 'soft'
+          }
+        })
+
+        expect(wrapper.find('.rounded-lg').exists()).toBe(true)
+      })
+
+      it('should be square when props.shape is square', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            shape: 'square'
+          }
+        })
+
+        expect(wrapper.find('.rounded-none').exists()).toBe(true)
+      })
+    })
+
+    describe('When size prop changes', () => {
+      it('should be xs when props.size is xs', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            size: 'xs'
+          }
+        })
+
+        expect(wrapper.find('.vk-badge__content').classes()).toContain('text-[0.6321875rem]')
+      })
+
+      it('should be sm when props.size is sm', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            size: 'sm'
+          }
+        })
+
+        expect(wrapper.find('.vk-badge__content').classes()).toContain('text-[0.74375rem]')
+      })
+
+      it('should be md when props.size is md', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            size: 'md'
+          }
+        })
+
+        expect(wrapper.find('.vk-badge__content').classes()).toContain('text-sm')
+      })
+
+      it('should be lg when props.size is lg', () => {
+        wrapper = mount(VkBadge, {
+          props: {
+            size: 'lg'
+          }
+        })
+
+        expect(wrapper.find('.vk-badge__content').classes()).toContain('text-[1.00625rem]')
+      })
+    })
+
+    describe('Slots', () => {
+      it('should display the content given', () => {
+        wrapper = mount(VkBadge, {
+          slots: {
+            default: 'Hello World'
+          }
+        })
+
+        expect(wrapper.find('.vk-badge').text()).toContain('Hello World')
+      })
+    })
+  })
+})
---
+++ packages/valko-ui-components/src/components/Badge/Badge.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { BadgeProps } from '@/components/Badge/interfaces'
+import useStyle from './Badge.styles'
+
+const props = withDefaults(defineProps<BadgeProps>(), {
+  content: '',
+  color: 'primary',
+  size: 'md',
+  shape: 'soft',
+  placement: 'top-right',
+  outlined: false,
+  dot: false,
+  hidden: false,
+  flat: false
+})
+
+defineOptions({ name: 'VkBadge' })
+
+const classes = useStyle(props)
+</script>
+
+<template>
+  <div :class="classes.container">
+    <span :class="classes.content">{{ !props.dot ? props.content : '' }}</span>
+    <slot />
+  </div>
+</template>
