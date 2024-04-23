cdb6e4b42f74c5acc5c055828770893373110095
Exequiel <80073050+BabyDr1v3r@users.noreply.github.com>
2024-04-22 21:35:20 -0300

    feat(vui-67): add notification component (#68)

--- packages/valko-ui-components/src/index.ts
+++ packages/valko-ui-components/src/index.ts
@@ -1,6 +1,7 @@
 import valkoui from './scripts/install'
 
 export * from './components'
+export * from './composables'
 export * from './types'
 export { default as createValkoUI } from './scripts/create'
 export default valkoui
--- packages/valko-ui-docs/src/pages/docs/DocsPage.vue
+++ packages/valko-ui-docs/src/pages/docs/DocsPage.vue
@@ -23,6 +23,7 @@ const menuItems: MenuItem[] = [
   { key: 'avatar', group: 'Ui', text: 'Avatar' },
   { key: 'badge', group: 'Ui', text: 'Badge' },
   { key: 'breadcrumbs', group: 'Ui', text: 'Breadcrumbs' },
+  { key: 'notification', group: 'Ui', text: 'Notification' },
   { key: 'progressbar', group: 'Ui', text: 'Progressbar' },
   { key: 'spinner', group: 'Ui', text: 'Spinner' },
   { key: 'tooltip', group: 'Ui', text: 'Tooltip' },
