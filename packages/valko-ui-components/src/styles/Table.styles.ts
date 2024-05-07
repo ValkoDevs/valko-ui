d0761c204719e868fbe80012388e7b982e40382f
Exequiel Segura <exequiel.segura@hotmail.com>
2024-05-07 17:01:52 -0300

    feat(vui-16): rebase.

--- packages/valko-ui-components/src/styles/Table.styles.ts
+++ packages/valko-ui-components/src/styles/Table.styles.ts
@@ -1,8 +1,6 @@
-import { computed } from 'vue'
-import { tv } from 'tailwind-variants'
-import { TableProps } from '#valkoui/types'
+import { tv, type TV } from 'tailwind-variants'
 
-const styles = tv({
+export default tv({
   slots: {
     table: [
       'vk-data-table',
@@ -539,16 +537,4 @@ const styles = tv({
       }
     }
   ]
-})
-
-export const useStyle = (props: TableProps) => computed(() => {
-  const slots = styles(props)
-  type Slot = keyof typeof slots
-
-  const keys = Object.keys(slots) as Slot[]
-
-  return keys.reduce((acc, key) => ({
-    ...acc,
-    [key]: slots[key]()
-  }), {}) as Record<Slot, string>
-})
+}) as unknown as ReturnType<TV>
