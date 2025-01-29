0cd79439fb03e01e7b543f7d076020aafc8cf5de
Exequiel <80073050+BabyDr1v3r@users.noreply.github.com>
2025-01-29 12:20:51 -0300

    ci(vui-127): update changeset summary.

--- packages/valko-ui-components/src/components/Card.vue
+++ packages/valko-ui-components/src/components/Card.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, useSlots } from 'vue'
+import { computed, defineSlots } from 'vue'
 import type { CardProps } from '#valkoui/types/Card'
 import useStyle from '#valkoui/composables/useStyle.ts'
 import styles from '#valkoui/styles/Card.styles.ts'
@@ -18,7 +18,11 @@ const props = withDefaults(defineProps<CardProps>(), {
 
 const emit = defineEmits(['click'])
 
+const slots = defineSlots<{ default(): Record<string, unknown>[] }>()
+
 const classes = useStyle<CardProps>(props, styles)
+
+const hasImageSlot = computed(() => slots.default?.().some((node) => node.type === VkCardImage))
 
 const element = computed(() => props.isPressable ? 'html:button' : 'div')
 
@@ -32,7 +36,7 @@ const onClick = () => {
     :is="element"
     :class="classes"
     :data-layout="layout"
-    :data-card-image="useSlots().default?.({}).some((node) => node.type === VkCardImage)"
+    :data-card-image="hasImageSlot"
     @click="onClick"
   >
     <slot />
