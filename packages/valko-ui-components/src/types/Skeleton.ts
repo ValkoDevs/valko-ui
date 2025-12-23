7e4188bf959d84689896e348904a5fef4bcaf19e
Exequiel <exequiel.segura@hotmail.com>
2025-12-23 15:43:28 -0300

    fix(vui-187): add missing semicolons.

--- packages/valko-ui-components/src/types/Skeleton.ts
+++ packages/valko-ui-components/src/types/Skeleton.ts
@@ -2,7 +2,7 @@ import type { Shapes } from './common'
 import type { SkeletonSlots } from '../styles/Skeleton.styles'
 
 export interface SkeletonProps extends Shapes {
-  width?: number
-  height?: number,
-  styleSlots?: Partial<SkeletonSlots>
+  width?: number;
+  height?: number;
+  styleSlots?: Partial<SkeletonSlots>;
 }
